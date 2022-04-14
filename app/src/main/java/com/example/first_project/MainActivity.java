7668a873988b8ac4f5bf8a3639b69e0d3820fafa
asci-00 <w_lan123@naver.com>
2022-04-14 22:11:12 +0900

    feat: WebView를 통한 webpage load

--- app/src/main/java/com/example/first_project/MainActivity.java
+++ app/src/main/java/com/example/first_project/MainActivity.java
@@ -4,46 +4,49 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText input_form;
-    private Button btn_test;
-    private TextView saved_text;
-    private String shared = "", text = "";
+    private WebView webView;
+    private String url = "https://google.com";
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
+            // 뒤로가기 처리
+            webView.goBack();
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        input_form = findViewById(R.id.input_form);   // xml에서 해당 id 값을 가진 view를 가져옴
-        btn_test = findViewById(R.id.btn_test);
-        saved_text = findViewById(R.id.saved_text);
+        webView = findViewById(R.id.webView);
 
-        SharedPreferences sharedPreferences = getSharedPreferences(shared, 0);
-        String value = sharedPreferences.getString("key", "");
-        saved_text.setText(value);
-
-        btn_test.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                text = input_form.getText().toString();
-            }
-        });
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl(url);
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.setWebViewClient(new WebViewClientClass());
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
-        SharedPreferences sharedPreferences = getSharedPreferences(shared, 0);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-
-        editor.putString("key", text);
-        editor.commit();
+    private class WebViewClientClass extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            // 현재 url을 통해 추가적인 기능을 구현하는 메서드
+            view.loadUrl(url);
+            return super.shouldOverrideUrlLoading(view, url);
+        }
     }
 }
