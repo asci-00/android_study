a11c4d27bdafe0cedc1af249455a53b83ad722de
asci-00 <w_lan123@naver.com>
2022-04-14 14:59:22 +0900

    feat: sample imageView 및 toast 기능 구현

--- app/src/main/java/com/example/first_project/MainActivity.java
+++ app/src/main/java/com/example/first_project/MainActivity.java
@@ -6,10 +6,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText et_id;
     Button btn_test;
+    ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +21,19 @@ public class MainActivity extends AppCompatActivity {
 
         et_id = findViewById(R.id.et_id);   // xml에서 해당 id 값을 가진 view를 가져옴
         btn_test = findViewById(R.id.btn_test);
+        img = findViewById(R.id.image);
 
         btn_test.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View view) {
                 et_id.setText("Custom Input Text");
+            }
+        });
+
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Sample toast message", Toast.LENGTH_SHORT).show();
             }
         });
     }
